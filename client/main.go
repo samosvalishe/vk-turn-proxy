e0bb125ba284cb202af791d8690e51a91beb2bd8
lastforathousandyears <165640975+lastForAThousandYears@users.noreply.github.com>
2026-03-27 23:23:15 +0500

    fix for new vk api

--- client/main.go
+++ client/main.go
@@ -78,7 +78,7 @@ func getVkCreds(link string) (string, string, string, error) {
 		}
 	}()
 
-	data := "client_secret=QbYic1K3lEV5kTGiqlq2&client_id=6287487&scopes=audio_anonymous%2Cvideo_anonymous%2Cphotos_anonymous%2Cprofile_anonymous&isApiOauthAnonymEnabled=false&version=1&app_id=6287487"
+	data := "client_id=6287487&token_type=messages&client_secret=QbYic1K3lEV5kTGiqlq2&version=1&app_id=6287487"
 	url := "https://login.vk.ru/?act=get_anonym_token"
 
 	resp, err := doRequest(data, url)
@@ -88,27 +88,7 @@ func getVkCreds(link string) (string, string, string, error) {
 
 	token1 := resp["data"].(map[string]interface{})["access_token"].(string)
 
-	data = fmt.Sprintf("access_token=%s", token1)
-	url = "https://api.vk.ru/method/calls.getAnonymousAccessTokenPayload?v=5.264&client_id=6287487"
-
-	resp, err = doRequest(data, url)
-	if err != nil {
-		return "", "", "", fmt.Errorf("request error:%s", err)
-	}
-
-	token2 := resp["response"].(map[string]interface{})["payload"].(string)
-
-	data = fmt.Sprintf("client_id=6287487&token_type=messages&payload=%s&client_secret=QbYic1K3lEV5kTGiqlq2&version=1&app_id=6287487", token2)
-	url = "https://login.vk.ru/?act=get_anonym_token"
-
-	resp, err = doRequest(data, url)
-	if err != nil {
-		return "", "", "", fmt.Errorf("request error:%s", err)
-	}
-
-	token3 := resp["data"].(map[string]interface{})["access_token"].(string)
-
-	data = fmt.Sprintf("vk_join_link=https://vk.com/call/join/%s&name=123&access_token=%s", link, token3)
+	data = fmt.Sprintf("vk_join_link=https://vk.com/call/join/%s&name=123&access_token=%s", link, token1)
 	url = "https://api.vk.ru/method/calls.getAnonymousToken?v=5.264"
 
 	resp, err = doRequest(data, url)
@@ -116,7 +96,7 @@ func getVkCreds(link string) (string, string, string, error) {
 		return "", "", "", fmt.Errorf("request error:%s", err)
 	}
 
-	token4 := resp["response"].(map[string]interface{})["token"].(string)
+	token2 := resp["response"].(map[string]interface{})["token"].(string)
 
 	data = fmt.Sprintf("%s%s%s", "session_data=%7B%22version%22%3A2%2C%22device_id%22%3A%22", uuid.New(), "%22%2C%22client_version%22%3A1.1%2C%22client_type%22%3A%22SDK_JS%22%7D&method=auth.anonymLogin&format=JSON&application_key=CGMMEJLGDIHBABABA")
 	url = "https://calls.okcdn.ru/fb.do"
@@ -126,9 +106,9 @@ func getVkCreds(link string) (string, string, string, error) {
 		return "", "", "", fmt.Errorf("request error:%s", err)
 	}
 
-	token5 := resp["session_key"].(string)
+	token3 := resp["session_key"].(string)
 
-	data = fmt.Sprintf("joinLink=%s&isVideo=false&protocolVersion=5&anonymToken=%s&method=vchat.joinConversationByLink&format=JSON&application_key=CGMMEJLGDIHBABABA&session_key=%s", link, token4, token5)
+	data = fmt.Sprintf("joinLink=%s&isVideo=false&protocolVersion=5&anonymToken=%s&method=vchat.joinConversationByLink&format=JSON&application_key=CGMMEJLGDIHBABABA&session_key=%s", link, token2, token3)
 	url = "https://calls.okcdn.ru/fb.do"
 
 	resp, err = doRequest(data, url)
